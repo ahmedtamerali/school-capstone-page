7933a66752c668738759869aad159a8e90dabebb
Ahmed Tamer <152633296+ahmedtamerali@users.noreply.github.com>
2025-09-07 00:59:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-
-const observer = new IntersectionObserver((entries) => {
+// Reveal cards animation when scrolled
+const sectionObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
+    console.log(entry.target, entry.isIntersecting, entry.intersectionRatio );
+    if (entry.isIntersecting && entry.intersectionRatio > 0) {
+      entry.target.classList.add('cards-show');
+    }
+  }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] });
+});
+
+document.querySelectorAll('.card, .TOC, .edp-heading, #EDP-timeline ol li, .deliverables-heading, #capstone-deliverables > ul > li').forEach(el => {
+  sectionObserver.observe(el);
+});
+
+// Reveal dividers animation when scrolled
+const dividerObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry.target, entry.isIntersecting, entry.intersectionRatio );
+    if (entry.isIntersecting && entry.intersectionRatio > 0) {
+      entry.target.classList.add('divider-show');
+    } 
+    else {
+      entry.target.classList.remove('divider-show');
     }
   });
 });
 
+document.querySelectorAll('.divider').forEach(el => {
+  dividerObserver.observe(el);
+});
 
-document.querySelectorAll('.card, .TOC, .divider.b').forEach((el) => observer.observe(el));
+
